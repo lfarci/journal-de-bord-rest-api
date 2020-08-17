a47487772cab20062de9951840a812a7157583c4
Logan Farci <logan.farci@outlook.be>
2020-08-17 21:03:54 +0200

    OAuth dependency added

---
+++ src/main/java/journal/de/bord/api/controllers/UsersRestController.java
@@ -0,0 +1,22 @@
+package journal.de.bord.api.controllers;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * The controller handles the REST interface exposing the users.
+ */
+@RestController
+public class UsersRestController {
+
+    @GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("name"));
+    }
+
+}
